ea555cf70089411038ad27b1166b89bf210dcaea
Pablo Gonzalez del Barrio <pablogb69@educastur.es>
2023-10-13 19:18:37 +0200

    Fase0

---
+++ src/main/java/pablogb/digestvuelos/DIGestVuelos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pablogb.digestvuelos;
+
+/**
+ *
+ * @author Charly Cimino
+ */
+public class DIGestVuelos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
